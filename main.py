130bb804ca6bafe4e88828bcd655451375ab57dc
AndrewKehr <andrew.kehr@gmail.com>
2025-02-25 12:28:09 -0600

    Added clock function

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import pygame
 from constants import *
 black = (255, 255, 255)
 white = (0, 0, 0)
+dt = 0
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}" )
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -18,6 +20,8 @@ def main():
                 return
         screen.fill(white)
         pygame.display.flip()
+        clock.tick(60)
+        dt = (clock.tick(60)/1000)
 
 if __name__ == "__main__":
     main()
