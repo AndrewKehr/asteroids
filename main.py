717c6bf64ce6fb4027e3eaf967e4e1503e0e75f6
AndrewKehr <andrew.kehr@gmail.com>
2025-02-25 17:24:03 -0600

    Added bullets and bullet detection to asteroids

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shots
 
 def main():
     pygame.init()
@@ -13,7 +14,9 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
+    Shots.containers = (updatable, shots, drawable)
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable)
@@ -35,6 +38,11 @@ def main():
             if i.collision(player):
                 print("Game over!")
                 sys.exit()
+                
+            for shot in shots:
+                if i.collision(shot):
+                    shot.kill()
+                    i.kill()
         
         screen.fill("black")
         
