3f10434869e0c0eb8420e8facd3c663bbeced76c
AndrewKehr <andrew.kehr@gmail.com>
2025-02-25 16:11:08 -0600

    added containers and groups for updatable and drawable

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
-import pygame # type: ignore
+import pygame
 from constants import *
 from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    
-    
-    while(True):
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        #Fill screen with black and refresh the display
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for i in drawable:
+            i.draw(screen)
         pygame.display.flip()
-        
-        #limit display refresh to 60 FPS
-        dt = (clock.tick(60) / 1000)
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
