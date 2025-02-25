5419e6843c0d1c3b2473bf5e4cdedba6f85bca52
AndrewKehr <andrew.kehr@gmail.com>
2025-02-25 16:39:37 -0600

    added asteroid field logic

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 import pygame
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
 
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         updatable.update(dt)
+        
         screen.fill("black")
+        
         for i in drawable:
             i.draw(screen)
+        
         pygame.display.flip()
+        
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
