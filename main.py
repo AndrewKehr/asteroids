b6782e373dcb7e7b7dc651ed3349db9b2e3a1f0f
AndrewKehr <andrew.kehr@gmail.com>
2025-02-25 12:30:40 -0600

    Cleaned up

--- main.py
+++ main.py
@@ -5,23 +5,24 @@ import pygame
 from constants import *
 black = (255, 255, 255)
 white = (0, 0, 0)
-dt = 0
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}" )
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        #Fill screen with black and refresh the display
         screen.fill(white)
         pygame.display.flip()
-        clock.tick(60)
-        dt = (clock.tick(60)/1000)
+        
+        #limit display refresh to 60 FPS
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
